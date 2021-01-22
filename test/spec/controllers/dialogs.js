3398f4ce039b85f6237eced35cef29f43fdbbf91
Rodion <rodion.bezruk@devit-team.com>
2021-01-22 00:59:25 +0000

    Product is generated

--- test/spec/controllers/dialogs.js
+++ test/spec/controllers/dialogs.js
@@ -1,19 +1,20 @@
 'use strict';
-describe('Controller: DialogsCtrl', function () {
-  beforeEach(module('ngMaterialWeburger'));
-  var DialogsCtrl;
-  var scope;
-  var ngModel;
-  var mdDialog;
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    DialogsCtrl = $controller('DialogsCtrl', {
-      $scope: scope,
-      $mdDialog: mdDialog,
-      mdeModel: ngModel,
-      style: {}
+describe('Controller: DialogsCtrl', function() {
+    beforeEach(module('ngMaterialWeburger'));
+    var DialogsCtrl;
+    var scope;
+    var ngModel;
+    var mdDialog;
+    beforeEach(inject(function($controller, $rootScope) {
+	scope = $rootScope.$new();
+	DialogsCtrl = $controller('DialogsCtrl', {
+	    $scope : scope,
+	    $mdDialog : mdDialog,
+	    wbModel : ngModel,
+	    style : {}
+	});
+    }));
+    it('should attach a list of awesomeThings to the scope', function() {
+	expect(DialogsCtrl).not.toBe(null);
     });
-  }));
-  it('should attach a list of awesomeThings to the scope', function () {
-  });
 });
